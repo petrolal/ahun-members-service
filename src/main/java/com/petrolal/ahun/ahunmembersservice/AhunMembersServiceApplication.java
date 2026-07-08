c4975e19b5843ead7f0f53e48dcf56bf28e76541
petrolal <petrolalucas@gmail.com>
2026-07-07 23:39:41 -0300

    Fix: Remove schedule tasks

--- src/main/java/com/petrolal/ahun/ahunmembersservice/AhunMembersServiceApplication.java
+++ src/main/java/com/petrolal/ahun/ahunmembersservice/AhunMembersServiceApplication.java
@@ -2,10 +2,8 @@ package com.petrolal.ahun.ahunmembersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class AhunMembersServiceApplication {
 
     public static void main(String[] args) {
